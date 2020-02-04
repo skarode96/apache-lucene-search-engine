94aca8cb3f0dc02384807c8233056321037e2b88
skarode96 <skarode96@gmail.com>
2020-02-04 17:48:28 +0000

    Apache lucene initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.apache.lucene.queryparser.classic.ParseException;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
