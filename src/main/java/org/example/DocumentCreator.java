1e4964f39f2d3e887d2a05b6dc30a597c86ce6af
skarode96 <skarode96@gmail.com>
2020-02-21 21:15:44 +0000

    adding dummy doc creator

--- src/main/java/org/example/DocumentCreator.java
+++ src/main/java/org/example/DocumentCreator.java
@@ -4,6 +4,7 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,5 +21,15 @@ public class DocumentCreator {
             return document;
         }).collect(Collectors.toList());
         return documentList;
+    }
+
+    public static List<Document> createDummyDocs() {
+        ArrayList<Document> documents = new ArrayList<>();
+        Document document = new Document();
+        document.add(new TextField("Firstname", "Sam", Field.Store.YES));
+        document.add(new TextField("Lastname", "Karode", Field.Store.YES));
+        documents.add(document);
+        documents.add(document);
+        return documents;
     }
 }
